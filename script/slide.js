5ff440b0d89320ac94c12e70b00c43e70297095f
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-10-08 14:00:23 -0600

    updates

---
+++ script/slide.js
@@ -0,0 +1,32 @@
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right');
+const leftBtn = document.getElementById('left');
+
+// Images are from unsplash
+let pictures = ['../images/background-pink.svg','../images/background-teal.svg'];
+
+img.src = pictures[0];
+let position = 0;
+
+const moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+}
+
+const moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
