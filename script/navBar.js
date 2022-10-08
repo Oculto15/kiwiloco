3e94132e5d61abbbf985f9c485518dcb53e755c2
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-10-08 14:32:00 -0600

    updates

--- script/navBar.js
+++ script/navBar.js
@@ -11,6 +11,23 @@ burger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
 
-item1.addEventListener('click', e => {
-  subMenu1.classList.add('open');
-});
+// item1.addEventListener('click', e => {
+//   subMenu1.classList.add('open');
+// });
+
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop < 80 || document.documentElement.scrollTop > 80) {
+//         document.querySelector(".navbar").style.padding = "30px 10px";
+//         document.querySelector(".navbar").style.opacity = "0.5";
+//         document.getElementById("logo").style.fontSize = "25px";
+//     } else {
+//         document.querySelector(".navbar").style.padding = "80px 10px";
+//         document.querySelector(".navbar").style.fontSize = "35px";
+//         document.getElementById("navbar").style.opacity = "1";
+//     }
+// }
