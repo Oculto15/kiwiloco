9161cf6eea0c193c2a087432b567bf5e69805986
Matias Villar <95631527+matvillar@users.noreply.github.com>
2022-10-08 11:34:15 -0600

    navbar

--- script/navBar.js
+++ script/navBar.js
@@ -1,7 +1,14 @@
 const burger = document.querySelector('.burger-menu');
 const navMenu = document.querySelector('.nav-menu');
+const subMenu1 = document.querySelector('.submenu');
+const subMenuItem = document.querySelector('.nav-item');
+const item1 = document.getElementById('submenu');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   navMenu.classList.toggle('active');
+});
+
+item1.addEventListener('click', e => {
+  subMenu1.classList.add('open');
 });
